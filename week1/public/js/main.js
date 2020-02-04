be26eb7a64c63c8adf0128ee07e22c4c77d32487
Roy Kuijper <roykuijper199716@hotmail.com>
2020-02-04 15:51:33 +0100

    load more finally works

--- week1/public/js/main.js
+++ week1/public/js/main.js
@@ -12,29 +12,68 @@ import createCard from '../modules/createCard.js';
     // Set Attributes
     container.setAttribute('class', 'container')
 
-
     // appendChild
     main.appendChild(container)
 
-    // Fetching API call    
-    Fetcher.get(url)
+    showResults(url)
+
+    function showResults(url){
+        return Fetcher.get(url)
         .then(({results:gameResults, next} ) => {
             gameResults.forEach(data => {  
                 container.appendChild(createCard(data, container))
             })
-            const button = document.createElement('button')
-            button.setAttribute('id', 'loadMore')
-            button.innerText = "loadMore"
-            button.addEventListener('click', () => {
-                Fetcher.get(next).then(({results:gameResults}) => {
-                    gameResults.forEach(data => {  
-                        container.appendChild(createCard(data, container))
-                    })
-                })
-            });
-            main.appendChild(button)
-            console.log(button)
+            return next;
         })
+        .then((next) => {
+            console.log(next)
+            const button = document.getElementById("loadMore")
+            if(button && next) {
+                // button exists
+                // button needs to be updated
+                button.setAttribute('value', next)
+                console.log(next)
+            } else if (next){
+                // button does not exists
+                // button needs to be made
+
+                const button = document.createElement('button')
+                button.setAttribute('id', 'loadMore')
+                button.setAttribute('value', next)
+                button.innerText = "loadMore"
+
+                button.addEventListener('click', (event) => {
+                    showResults(event.target.value)
+                })
+
+                main.appendChild(button)
+            } else {
+                document.getElementById("button-2").remove();
+            }
+        })
+    }
+    // Fetching API call    
+    // Fetcher.get(url)
+    //     .then(({results:gameResults, next} ) => {
+    //         gameResults.forEach(data => {  
+    //             container.appendChild(createCard(data, container))
+    //         })
+    //         return next;
+    //     })
+    //     .then((data) => console.log(data))
+            // const button = document.createElement('button')
+            // button.setAttribute('id', 'loadMore')
+            // button.innerText = "loadMore"
+            // button.addEventListener('click', () => {
+            //     Fetcher.get(next).then(({results:gameResults}) => {
+            //         gameResults.forEach(data => {  
+            //             container.appendChild(createCard(data, container))
+            //         })
+            //     })
+            // });
+            // main.appendChild(button)
+            // console.log(button)
+        // })
             
 
 }
